2448e2f1f72fb50e1a8a9b7257093ef7eb0d6ac6
imrohan15 <sehgalrohan619@gmail.com>
2022-02-03 17:39:36 +0530

    Hello World - Commit from eclipse

---
+++ HelloWorldDemo/src/HelloWorldDemo/main/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package HelloWorldDemo.main;
+
+/**
+ * @author HP
+ *
+ */
+public class HelloWorld {
+ 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone, Welcome");
+
+	}
+
+}
